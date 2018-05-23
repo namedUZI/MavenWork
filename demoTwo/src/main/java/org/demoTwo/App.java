ab7f63d0d1aeea6ca3bfb83386dbc8b4d20ffe97
namedUZI <2792889381@qq.com>
2018-05-23 17:39:13 +0800

    Maven聚合项目

---
+++ demoTwo/src/main/java/org/demoTwo/App.java
@@ -0,0 +1,13 @@
+package org.demoTwo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
